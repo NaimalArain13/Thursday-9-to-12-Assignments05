904ecc75e8f54abc1b1635177a3b9df39ec243c5
NaimalArain13 <naimalarain13@gmail.com>
2024-11-04 19:55:44 +0500

    assignment 05 updated

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Banner from "@/components/Banner/Banner";
+import Navbar from "@/components/Navbar/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+    <Navbar />
+    <Banner />
+    </div>
+  );
+}
---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="h-[20%] bg-[#A29875] flex items-center">
+      <div className="h-[87px] flex items-center">
+        <div className="w-[242px] flex py-[11] items-center text-white px-7">
+          <h1>
+            <Image src={"/images/logo.png"} alt="logo" width={63} height={70} />
+          </h1>
+          <h1 className=" w-[174px] h-[83px] flex items-center font-serif text-[50px]">
+            urity
+          </h1>
+        </div>
+        <div className="h-14 w-[769px] px-3 top-[39px] left-[431px] rounded-[13px] bg-neutral-100  hidden md:flex justify-between items-center">
+          <p className="text-sm ">
+            Search for Gold Jewellery, Diamond Jewellery and more…
+          </p>
+          <p className="w-[21px] h-[21px] ">
+            <Image
+              src={"/images/search.png"}
+              alt="search-icon"
+              width={19}
+              height={19}
+            />
+          </p>
+        </div>
+        <div className="w-[177px] h-[30px] top-13 ml-28">
+          <div className="hidden md:flex gap-[38px]">
+            <p>
+              <Image
+                src={"/images/heart.png"}
+                alt="heart-icon"
+                width={33}
+                height={29}
+              />
+            </p>
+            <p>
+              <Image
+                src={"/images/user.png"}
+                alt="user-icon"
+                width={29}
+                height={28}
+              />
+            </p>
+            <p>
+              <Image
+                src={"/images/cart.png"}
+                alt="cart-icon"
+                width={38}
+                height={30}
+              />
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+
