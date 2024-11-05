47a0650434919b19f28e5de947ac0c012f4e79b9
NaimalArain13 <naimalarain13@gmail.com>
2024-11-05 21:10:11 +0500

    removed all unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,84 +1,10 @@
-// import React from "react";
-// import Navbar from "@/components/Navbar/Navbar";
-
-// export default function Home() {
-//   return (
-//     <div className="h-screen">
-//       <Navbar />
-
-//       <div className="flex flex-col gap-y-5 w-full max-w-[1486px] md:flex-row md:justify-between items-center h-[80%]  md:m-14">
-      
-//         <div
-//           className="w-full max-w-[421px] md:h-[573px]  md:hidden justify-center items-center bg-cover bg-center rounded-tl-[128px] rounded-br-[128px]"
-//           style={{ backgroundImage: `url('/images/banner.png')`}}
-//         >
-//           <div className="w-[380px] h-[525px] border-[3px] m-5 border-white rounded-tl-[128px] rounded-br-[128px]"></div>
-//         </div>
-//         <div className="h-[573px] w-[1217px] flex justify-between">
-//         <div className="md:w-1/2  flex gap-y-3 p-4 flex-col md:pt-3 justify-center items-center md:gap-y-5 md:items-start">
-//           <h1 className={`text-3xl md:text-[44px] leading-snug md:h-[162px] font-Roboto font-bold`}>
-//             IMPECCABLE CRAFTSMANSHIP AND FINESSE
-//           </h1>
-//           <p className="md:w-[798px] md:h-36 mb-2 font-medium  text-md md:text-[29px] text-[#787054]">
-//             An example of intricate workmanship and detail, elegant necklaces
-//             and long and short chains form a part of our desirable collection.
-//           </p>
-//           <button className="md:text-[25px] text-lg font-medium w-md md:w-[274px] md:h-[49px] bg-[#A29875] text-white px-5 py-2 md:p-2 rounded-lg">
-//             {" "}
-//             Explore Now
-//           </button>
-//         </div>
-//         <div
-//           className="hidden md:w-[421px] h-[573px] md:flex justify-center items-center bg-cover bg-center "
-//           style={{ backgroundImage: `url('/images/banner.png')` }}
-//         >
-//           <div className="w-[380px] h-[525px] border-[3px] border-white m-5 rounded-tl-[128px] rounded-br-[128px]"></div>
-//         </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
 import React from "react";
-// import Navbar from "@/components/Navbar/Navbar";
 import Banner from "@/components/Banner/Banner";
 
 export default function Home() {
   return (
     <div className="h-screen">
-      {/* <Navbar /> */}
       <Banner />
-
-      {/* <div className="flex flex-col gap-y-5 w-full md:flex-row md:justify-between h-[80%] m-5 md:m-14">
-        <div
-          className="w-full max-w-[421px] md:h-[573px]  md:hidden justify-center items-center bg-cover bg-center rounded-tl-[128px] rounded-br-[128px]"
-          style={{ backgroundImage: `url('/images/banner.png')`}}
-        >
-          <div className="w-[380px] h-[525px] border-[3px] m-5 border-white rounded-tl-[128px] rounded-br-[128px]"></div>
-        </div>
-        <div className="h-[573px] w-[1217px] flex justify-between m-6">
-        <div className="md:w-1/2 flex gap-y-3 p-4 flex-col md:pt-3 justify-center items-center md:gap-y-5 md:items-start">
-          <h1 className={`text-3xl md:text-[44px] leading-snug md:h-[162px] font-Roboto font-bold`}>
-            IMPECCABLE CRAFTSMANSHIP AND FINESSE
-          </h1>
-          <p className="md:w-[798px] md:h-36 mb-2 font-medium  text-md md:text-[29px] text-[#787054]">
-            An example of intricate workmanship and detail, elegant necklaces
-            and long and short chains form a part of our desirable collection.
-          </p>
-          <button className="md:text-[25px] text-lg font-medium w-md md:w-[274px] md:h-[49px] bg-[#A29875] text-white px-5 py-2 md:p-2 rounded-lg">
-            {" "}
-            Explore Now
-          </button>
-        </div>
-        <div
-          className="hidden w-[421px] h-[573px] md:flex justify-center items-center bg-cover bg-center "
-          style={{ backgroundImage: `url('/images/banner.png')` }}
-        >
-          <div className="w-[380px] h-[525px] border-[3px] border-white m-5 rounded-tl-[128px] rounded-br-[128px]"></div>
-        </div>
-        </div>
-      </div> */}
     </div>
   );
 }
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -4,7 +4,6 @@ import React from "react";
 export default function Navbar() {
   return (
     <div className="h-[70px] bg-[#A29875] flex items-center">
-      {/* <div className="h-[87px] flex items-center"> */}
         <div className="md:w-[242px] w-full flex py-[11] items-center  text-white px-2 md:px-7">
             <Image src={"/images/logo.png"} alt="logo" width={50} height={50} />
           <h1 className=" w-full h-[83px] flex items-center font-serif text-[50px]">
@@ -52,30 +51,6 @@ export default function Navbar() {
             </p>
           </div>
         </div>
-      {/* </div> */}
     </div>
   );
 }
-
-{/* <div className="h-[70px] bg-[#A29875] flex items-center justify-between px-2 md:px-7">
-  <div className="w-full md:w-[242px] flex items-center text-white">
-    <Image src={"/images/logo.png"} alt="logo" width={50} height={50} />
-    <h1 className="flex-1 font-serif text-[24px] md:text-[50px]">urity</h1>
-  </div>
-  <div className="hidden md:flex items-center w-full max-w-md bg-neutral-100 lg:max-w-lg">
-    <input
-      type="text"
-      placeholder="Search for Gold Jewellery, Diamond Jewellery and more…"
-      className="flex-1 px-4 text-sm text-gray-600 bg-transparent outline-none"
-    />
-    <Image src={"/images/search.png"} alt="search-icon" width={20} height={20} />
-  </div>
-  <div className="flex gap-3 md:gap-[38px]">
-    <Image src={"/images/heart.png"} alt="heart-icon" width={25} height={25} />
-    <Image src={"/images/user.png"} alt="user-icon" width={25} height={25} />
-    <Image src={"/images/cart.png"} alt="cart-icon" width={25} height={25} />
-  </div>
-</div> */}
-
-
-
